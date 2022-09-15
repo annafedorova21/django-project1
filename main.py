7418a3a2e6d16bba6d6e99ff346ef326dc9c8954
Anna <102650792+annafedorova21@users.noreply.github.com>
2022-09-15 11:44:32 +0300

    added Readme

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-# This is a sample Python script.
+This is my first Django project!
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+It is a web-site with projects, that authorized users can add and share with other developers.
